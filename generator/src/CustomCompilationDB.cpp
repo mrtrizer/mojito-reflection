ff8b24301191afa967a3fabe4292e9a2ef6c77cc
Denis Zdorovtsov <mrtrizer@gmail.com>
2019-01-26 12:31:06 +0300

    Fixed includes in compilation db.

--- generator/src/CustomCompilationDB.cpp
+++ generator/src/CustomCompilationDB.cpp
@@ -1,8 +1,54 @@
 #include "CustomCompilationDB.hpp"
 
+#include <iostream>
+#include <regex>
+
 #include "CompillerArgs.hpp"
 
-#include <iostream>
+#include <boost/process.hpp>
+#include <boost/algorithm/string.hpp>
+
+static std::vector<boost::filesystem::path> getClangIncludeDirs(const boost::filesystem::path& compillerPath) {
+    using namespace boost;
+
+    process::ipstream pipe_stream;
+    
+    std::string command = compillerPath.string() + " -v";
+    
+    process::child c(command, process::std_err > pipe_stream);
+
+    std::vector<boost::filesystem::path> includeDirs;
+
+    std::string clangInstallDir;
+    std::string version;
+
+    std::string line;
+    while (!pipe_stream.eof() && std::getline(pipe_stream, line) && !line.empty()) {
+        trim(line);
+        
+        std::cmatch m;
+        
+        std::regex re("version ([0-9]*?\\.[0-9]*?\\.[0-9]*)");
+        
+        if (std::regex_search(line.c_str(), m, re))
+            version = m[1];
+            
+        const char installedDirMarker[] = "InstalledDir: ";
+        if (line.find(installedDirMarker) == 0)
+            clangInstallDir = line.substr(sizeof(installedDirMarker) / sizeof(installedDirMarker[0]) - 1);
+    }
+    
+    if (version.empty())
+        throw std::runtime_error("Can't get clang version by path " + compillerPath.string());
+    
+    if (clangInstallDir.empty())
+        throw std::runtime_error("Can't access clang by path " + compillerPath.string());
+
+    includeDirs.emplace_back(clangInstallDir + "/../include/c++/v1").normalize();
+    includeDirs.emplace_back(clangInstallDir + "/../lib/clang/" + version + "/include").normalize();
+
+    return includeDirs;
+}
 
 CustomCompilationDatabase::CustomCompilationDatabase(const boost::filesystem::path& compillerPath, const CompillerArgs& compillerArgs)
     : m_compillerArgs(compillerArgs)
@@ -27,6 +73,12 @@ std::vector<clang::tooling::CompileCommand> CustomCompilationDatabase::getAllCom
     std::vector<clang::tooling::CompileCommand> commands;
     auto arguments = m_compillerArgs.clangArguments();
     arguments.insert(arguments.begin(), "c++");
+    auto includes = getClangIncludeDirs(m_compillerPath);
+    for (const auto& include : includes) {
+        arguments.push_back("-I");
+        arguments.push_back(include.string());
+    }
+    //arguments.push_back("-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1");
     for (const auto& arg : arguments)
         std::cout << arg << ' ';
     std::cout << std::endl;
